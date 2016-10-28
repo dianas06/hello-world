caa839398887ac5114242e4eaf802402a9fa2901
diana <dsouzadiana527@gmail.com>
2016-10-28 17:39:10 +0530

    committing developer1 changes

--- helloGit/src/helloGit/Test.java
+++ helloGit/src/helloGit/Test.java
@@ -2,7 +2,8 @@ package helloGit;
 
 public class Test {
 	 public static void main (String[] args) {
-			System.out.println ("Hello baal!");
+			System.out.println ("Hello baal developer1");
+			System.out.println ("dev1");
 		    }
 
 }
